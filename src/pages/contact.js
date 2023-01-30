09e120fa15027ea7be4edd7d5539d0c9cd7994a6
Hugochon <hugo.schneegans@gmail.com>
2023-01-30 16:28:14 +0100

    quick modification

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from '../styles/Contact.module.css'
+import styles from '../styles/contact.module.css'
 import Header from './header'
 
 export default function Contact(){
